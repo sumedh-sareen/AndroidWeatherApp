5536ec216db2bea0d1728bb153adfb284e479a5e
sumedh-sareen <ssk656@gmail.com>
2025-08-11 11:06:24 +1000

    master branch - basic structure - initial stage

---
+++ app/src/main/java/com/example/androidweatherapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.androidweatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.androidweatherapp.ui.theme.AndroidWeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
+
+
